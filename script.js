6cb4e3f354cc21df44f07824b3c3ff959730cc25
BOT22345 <nmongia48@gmail.com>
2024-09-12 19:01:10 +0530

    added the basic animation of character movement on key down space bar and up arrow

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.onkeydown=function(e){
     console.log("key code is ",e.keyCode);
+    if(e.keyCode==38 || e.keyCode==32){
+        mario=document.querySelector(".mario")
+        mario.classList.add("animateMario")
+        setTimeout(()=>{
+            mario.classList.remove("animateMario");
+        },1000)
+    }
 }
+
+setInterval(()=>{
+    mario=document.querySelector(".mario")
+    obstacle=document.querySelector(".obstacle");
+    gameOver=document.querySelector(".gameOver");
+
+    dx=window.getComputedStyle(mario,null).getPropertyValue('left')
+    dy=window.getComputedStyle(mario,null).getPropertyValue('top');
+    
+    ox=window.getComputedStyle(obstacle,null).getPropertyValue('left');
+    oy=window.getComputedStyle(obstacle,null).getPropertyValue('top');
+    console.log(ox);
+},100)
